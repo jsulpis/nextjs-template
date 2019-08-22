01466dff9f0bd558d83a38bebc72766f8ea8a46f
Julien SULPIS <julien.sulpis@gmail.com>
2019-08-22 13:25:15 +0200

    chore: add bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
-module.exports = {
+const withBundleAnalyzer = require("@zeit/next-bundle-analyzer");
+
+const nextConfig = {
+  analyzeServer: ["server", "both"].includes(process.env.BUNDLE_ANALYZE),
+  analyzeBrowser: ["browser", "both"].includes(process.env.BUNDLE_ANALYZE),
+  bundleAnalyzerConfig: {
+    server: {
+      analyzerMode: "static",
+      reportFilename: "../bundles/server.html"
+    },
+    browser: {
+      analyzerMode: "static",
+      reportFilename: "bundles/client.html"
+    }
+  },
   webpack: config => {
     // Fixes npm packages that depend on `fs` module
     config.node = {
@@ -8,3 +22,5 @@ module.exports = {
     return config;
   }
 };
+
+module.exports = withBundleAnalyzer(nextConfig);
