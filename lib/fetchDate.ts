642c57ebf0a7e8e604e3034fb3b4395faa0a5a33
Julien SULPIS <julien.sulpis@gmail.com>
2019-09-14 15:56:24 +0200

    feat: make helper function for API calls

--- lib/fetchDate.ts
+++
@@ -1,6 +0,0 @@
-import fetch from "isomorphic-unfetch";
-
-export default async function fetchDate(): Promise<string> {
-  const res = await fetch("/api/date");
-  return await res.json().then(payload => payload.date);
-}
