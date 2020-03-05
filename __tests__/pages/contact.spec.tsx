f35a5a90c1abf8c867d21f4d5bf8be58154aec76
Julien Sulpis <julien.sulpis@gmail.com>
2020-03-05 21:14:29 +0100

    fix lint et ajoute le dossier .idea au .gitignore

--- __tests__/pages/contact.spec.tsx
+++ __tests__/pages/contact.spec.tsx
@@ -9,9 +9,7 @@ describe("With React Testing Library", () => {
   it('Shows "This is the Contact page"', () => {
     const { container } = render(<ContactPage />);
 
-    expect(container.querySelector("h1").textContent).toBe(
-      "This is the Contact page"
-    );
+    expect(container.querySelector("h1").textContent).toBe("This is the Contact page");
   });
 
   it("should send a Google Analytics event when submitting the form", () => {
