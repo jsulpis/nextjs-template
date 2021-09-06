5cc99115df19f9ca944e96cdad579d19711aa8e4
Julien Sulpis <julien.sulpis@gmail.com>
2021-09-06 19:23:13 +0200

    feat: remove Google Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,5 @@
 import App from "next/app";
-import Router from "next/router";
 import "../styles/styles.scss";
 import "@fortawesome/fontawesome-svg-core/styles.css";
-
-import * as gtag from "lib/gtag";
-
-Router.events.on("routeChangeComplete", url => gtag.pageview(url));
 
 export default App;
